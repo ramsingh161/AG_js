1ad78fa450c9eccfad7a9ac670ef8c1c2450a434
ramsingh161 <ramsingh15777@gmail.com>
2026-02-12 14:31:21 +0530

    push

--- rext.js
+++ rext.js
@@ -454,23 +454,23 @@
 //     });
 
 
+//fake pyhtin code just for pushing in git
+// n = int(input().strip())
+// arr = list(map(int, input().split()))
 
-n = int(input().strip())
-arr = list(map(int, input().split()))
+// if len(arr) != n:
+//     print(-1)
+// else:
+//     max_val = max(arr)
+//     count = [0] * (max_val + 1)
 
-if len(arr) != n:
-    print(-1)
-else:
-    max_val = max(arr)
-    count = [0] * (max_val + 1)
+//     for num in arr:
+//         count[num] += 1
 
-    for num in arr:
-        count[num] += 1
+//     result = []
+//     for i in range(len(count)):
+//         result.extend([i] * count[i])
 
-    result = []
-    for i in range(len(count)):
-        result.extend([i] * count[i])
-
-    print(' '.join(map(str, result)))
+//     print(' '.join(map(str, result)))
 
 
