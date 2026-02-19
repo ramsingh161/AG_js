01797dd7cdb284464c4b6c976f6e77446e6e3ea9
ramsingh161 <ramsingh15777@gmail.com>
2026-02-19 12:28:56 +0530

    push

--- 1.dart
+++ 1.dart
@@ -75,10 +75,51 @@ void main() {
   // String lname = "doe";
   // print("$fname $lname");
 
-  print("first number");
-  double num1 = double.parse(stdin.readLineSync()!);
-  print("second number");
-  double num2 = double.parse(stdin.readLineSync()!);
-  double sum = num1 + num2;
-  print(sum);
+  // print("first number");
+  // double num1 = double.parse(stdin.readLineSync()!);
+  // print("second number");
+  // double num2 = double.parse(stdin.readLineSync()!);
+  // double sum = num1 + num2;
+  // print(sum);
+
+  // print("age:");
+  // int age = int.parse(stdin.readLineSync()!);
+  // if(age >= 18){
+  //   print("you are eligible to vote");
+  // }
+  // else {
+  //   print("you are not eligible to vote");
+  //   }
+
+  // List<String> names = ["john", "doe", "smith"];
+  // for(int i = 0; i < names.length; i++){
+  //   print(names[i]);
+  //   print(names.length);
+  // }
+
+
+  //electricity consumption program 
+  // unit range 0-100 = 1.5 rs/unit
+  // unit range 101-200 = 2.5 rs/unit
+  // unit range 201-300 = 4.0 rs/unit
+  // unit range above 300 = 5.0 rs/unit
+  // print("enter the number of units consumed:");
+  // int u = int.parse(stdin.readLineSync()!);
+  // double bill;
+  // if(u <= 100){
+  //   bill = u * 1.5;
+  // }
+  // else if(u <= 200){
+  //   bill = (100 * 1.5) + ((u - 100) * 2.5);
+  // }
+  // else if(u <= 300){
+  //   bill = (100 * 1.5) + (100 * 2.5) + ((u - 200) * 4.0);
+  // }
+  // else {
+  //   bill = (100 * 1.5) + (100 * 2.5) + (100 * 4.0) + ((u - 300) * 5.0);
+  // }
+  // print(bill);
+
+
+  
 }
